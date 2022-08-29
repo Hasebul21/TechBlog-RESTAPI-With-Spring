6679b1016e1ad6f5c73204369de9225f270f7255
unknown <hasebulhassan21@gmail.com>
2022-08-29 13:20:31 +0600

    Setup project with application properties

---
+++ src/test/java/com/example/techblogapi/TechBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
