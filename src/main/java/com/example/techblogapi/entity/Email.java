b73090248a77224f94c8360c3fcbd4f543d2167e
unknown <hasebulhassan21@gmail.com>
2022-08-29 19:22:37 +0600

    add email and password validation

--- src/main/java/com/example/techblogapi/entity/Email.java
+++
@@ -1,4 +0,0 @@
-package com.example.techblogapi.entity;
-
-public @interface Email {
-}
--- src/main/java/com/example/techblogapi/repository/UserRepository.java
+++ src/main/java/com/example/techblogapi/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.example.techblogapi.repository;
 
 import com.example.techblogapi.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends CrudRepository<User,Integer> {
 
     public User findByEmail(String email);
     public Boolean existsByEmail(String email);
