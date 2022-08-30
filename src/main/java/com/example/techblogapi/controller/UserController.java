c2ccb98651b13c880da97c1805f547006106de74
unknown <hasebulhassan21@gmail.com>
2022-08-30 08:37:14 +0600

    make some changes

--- src/main/java/com/example/techblogapi/controller/UserController.java
+++ src/main/java/com/example/techblogapi/controller/UserController.java
@@ -5,8 +5,10 @@ import com.example.techblogapi.entity.User;
 import com.example.techblogapi.repository.UserRepository;
 import com.example.techblogapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,34 +19,33 @@ public class UserController {
 
 
      @RequestMapping(method = RequestMethod.GET, value = "/users")
-     public Iterable<User> getUser() {
+     public ResponseEntity<List<User>> getAllUser() {
 
-          return userService.getUser();
+          return userService.getAllUser();
      }
 
      @RequestMapping(method = RequestMethod.GET, value = "/users/{id}")
-     public Optional<User> getSingleUser(@PathVariable int id) {
+     public ResponseEntity<User> getSingleUser(@PathVariable int id) {
 
           return userService.getSingleUser(id);
      }
 
      @RequestMapping(method = RequestMethod.POST,value = "/users")
-     public void addUser(@RequestBody User user)  {
+     public ResponseEntity<String> addUser(@RequestBody User user)  {
 
-          System.out.println(user.getEmail());
-          userService.addUser(user);
+          return userService.addUser(user);
      }
 
      @RequestMapping(method = RequestMethod.PUT,value = "/users/{email}")
-     public void updateUser(@PathVariable String email,@RequestBody User user) {
+     public ResponseEntity<String> updateUser(@PathVariable String email,@RequestBody User user) {
 
-          userService.updateUser(email,user);
+          return userService.updateUser(email,user);
      }
 
      @RequestMapping(method = RequestMethod.DELETE,value = "/users")
-     public void updateUser(@RequestBody User user) {
+     public ResponseEntity<String> deleteUser(@RequestBody User user) {
 
-          userService.deleteUser(user.getId());
+          return userService.deleteUser(user.getId());
      }
 
 }
