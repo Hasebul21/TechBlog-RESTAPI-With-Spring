f563de8ea28c7e26b5ec7c2fc57c43d8b9d26b1a
unknown <hasebulhassan21@gmail.com>
2022-08-30 08:37:14 +0600

    make some changes

--- src/main/java/com/example/techblogapi/repository/UserRepository.java
+++ src/main/java/com/example/techblogapi/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
 
-    public User findByEmail(String email);
+    public Optional<User> findByEmail(String email);
     public Boolean existsByEmail(String email);
 
 }
--- src/main/java/com/example/techblogapi/service/UserService.java
+++ src/main/java/com/example/techblogapi/service/UserService.java
@@ -6,10 +6,15 @@ import com.example.techblogapi.repository.UserRepository;
 import org.apache.catalina.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,46 +23,63 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void addUser(User user)  {
+    public ResponseEntity<String> addUser(User user)  {
 
-        if(userRepository.existsByEmail(user.getEmail())==false) {
+        try {
 
-            try {
+            userRepository.save(user);
 
-                userRepository.save(user);
+        }catch(Exception err){
 
-            }catch(Exception er){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err.getMessage().toString());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully added entity");
 
-                System.out.println(er.getMessage().toString());
-            }
-
-        }else System.out.println("Already Exist");
     }
 
-    public Iterable<User> getUser() {
+    public ResponseEntity<List<User>> getAllUser() {
 
-        return userRepository.findAll();
+        List<User>allUser=new ArrayList<>();
+        userRepository.findAll().forEach(allUser::add);
+        return ResponseEntity.status(HttpStatus.OK).body(allUser);
     }
 
-    public Optional<User> getSingleUser(int id) {
+    public ResponseEntity<User> getSingleUser(int id) {
 
-        return userRepository.findById(id);
+        Optional<User> checkUser=userRepository.findById(id);
+        if(checkUser.isPresent()==false) {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        User existingUser=checkUser.get();
+        return ResponseEntity.status(HttpStatus.FOUND).body(existingUser);
+
     }
 
-    public void updateUser(String email,User user) {
+    public ResponseEntity<String> updateUser(String email,User user) {
 
         if(userRepository.existsByEmail(email)){
 
-           User user1=userRepository.findByEmail(email);
-           user1.setEmail(user.getEmail());
-           user1.setPassword(user.getPassword());
-           userRepository.save(user1);
+            Optional<User> checkUser=userRepository.findByEmail(email);
+            User newUser=checkUser.get();
+            newUser.setEmail(user.getEmail());
+            newUser.setPassword(user.getPassword());
+            userRepository.save(newUser);
+            return ResponseEntity.status(HttpStatus.OK).body("Sucessfully Updated");
         }
-        else System.out.println("NOT FOUND");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email Doesnot Exist");
     }
 
-    public void deleteUser(int id) {
+    public ResponseEntity<String> deleteUser(int id) {
 
-        userRepository.deleteById(id);
+        try {
+
+            userRepository.deleteById(id);
+
+        }catch(Exception err){
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err.getMessage().toString());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully Deleted entity");
     }
 }
