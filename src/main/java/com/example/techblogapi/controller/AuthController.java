ed08b4a43267c665839c1931c4d31cd8e22e2e5e
unknown <hasebulhassan21@gmail.com>
2022-08-31 18:57:50 +0600

    issue3 : implement auth controller

---
+++ src/main/java/com/example/techblogapi/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.techblogapi.controller;
+
+import com.example.techblogapi.entity.User;
+import com.example.techblogapi.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<? extends Object> signUp(@RequestBody User user){
+
+        try {
+
+            User newUser=authService.signUp(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+
+        }catch(Exception err){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err.getMessage().toString());
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<User> signIn(@RequestBody User user){
+
+        Optional<User>newUser=authService.signIn(user);
+        if(newUser.isPresent()) return ResponseEntity.status(HttpStatus.OK).body(newUser.get());
+        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+    }
+}
