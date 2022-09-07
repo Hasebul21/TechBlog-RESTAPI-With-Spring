be03e1fb4a273235b6bed71261e346b4d59c0028
unknown <hasebulhassan21@gmail.com>
2022-09-07 12:40:28 +0600

    implemented jwt

---
+++ src/main/java/com/example/techblogapi/security/Authenticate.java
@@ -0,0 +1,26 @@
+package com.example.techblogapi.security;
+
+import com.example.techblogapi.entity.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Component
+public class Authenticate {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsInfo userDetailsInfo;
+
+    public String authenticate(@RequestBody Users users){
+
+
+        final UserDetails userDetails=userDetailsInfo.loadUserByUsername(users.getEmail());
+        final String token= jwtUtil.generateToken(userDetails);
+        return token;
+
+    }
+}
