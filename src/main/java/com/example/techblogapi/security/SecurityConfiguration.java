3a803571d57124ebfab1f83feb4d62cf61717c4c
unknown <hasebulhassan21@gmail.com>
2022-09-09 15:16:16 +0600

    added in securityconfig

--- src/main/java/com/example/techblogapi/security/SecurityConfiguration.java
+++ src/main/java/com/example/techblogapi/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.techblogapi.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,8 @@ public class SecurityConfiguration {
                 .disable()
                 .authorizeRequests()
                 .antMatchers("/signin","/signup")
+                .permitAll()
+                .antMatchers(HttpMethod.GET,"/storys/","/storys/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
