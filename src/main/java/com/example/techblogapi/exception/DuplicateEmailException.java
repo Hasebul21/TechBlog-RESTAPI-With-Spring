0e5f53347e82ffd4fac034626faf8b3c1b29e4d6
unknown <hasebulhassan21@gmail.com>
2022-09-14 09:46:05 +0600

    added custom exception

---
+++ src/main/java/com/example/techblogapi/exception/DuplicateEmailException.java
@@ -0,0 +1,9 @@
+package com.example.techblogapi.exception;
+
+public class DuplicateEmailException extends RuntimeException{
+
+    public DuplicateEmailException(String message){
+
+        super(message);
+    }
+}
