87c603457c8f78dd7b3b88c33d5a78b7ad1099bf
unknown <hasebulhassan21@gmail.com>
2022-09-23 16:18:49 +0600

    tested auth service and controller

---
+++ src/test/java/com/example/techblogapi/controller/AuthControllerTest.java
@@ -0,0 +1,99 @@
+package com.example.techblogapi.controller;
+
+import com.example.techblogapi.entity.Users;
+import com.example.techblogapi.exception.DuplicateEmailException;
+import com.example.techblogapi.repository.UserRepository;
+import com.example.techblogapi.security.Authenticate;
+import com.example.techblogapi.security.JwtFilter;
+import com.example.techblogapi.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @MockBean
+    private Authenticate authenticate;
+
+    @MockBean
+    private UserRepository mockuserRepository;
+    @MockBean
+    private AuthService authService;
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc= MockMvcBuilders
+                .webAppContextSetup(context).addFilter(jwtFilter, "/*").build();
+    }
+
+    @Test
+    @DisplayName("POST/signup  Success")
+    void createUserSuccess() throws Exception{
+
+        Users user=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        mockMvc.perform(post("/api/v1/signup")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding("utf-8")
+                        .content(asJsonString(user)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("POST/signup  Failed")
+    void createUserFailed() throws Exception{
+
+        Users user=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        doThrow(DuplicateEmailException.class).when(authService).signUp(user);
+        mockMvc.perform(post("/api/v1/signup")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding("utf-8")
+                        .content(asJsonString(user)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    @DisplayName("POST/signin Success")
+    void loginUserScuess() throws Exception{
+
+        Users user=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        when(authService.signIn(user)).thenReturn(user);
+        mockMvc.perform(post("/api/v1/signin")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding("utf-8")
+                        .content(asJsonString(user)))
+                .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/example/techblogapi/service/AuthServiceTest.java
@@ -0,0 +1,78 @@
+package com.example.techblogapi.service;
+
+import com.example.techblogapi.Utils.PasswordValidator;
+import com.example.techblogapi.dto.UserDto;
+import com.example.techblogapi.entity.Users;
+import com.example.techblogapi.exception.AccessDeniedException;
+import com.example.techblogapi.exception.InvalidPasswordException;
+import com.example.techblogapi.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+    @MockBean
+    private PasswordValidator mockpasswordValidator;
+    @MockBean
+    private PasswordEncoder mockpasswordEncoder;
+    @MockBean
+    private UserRepository mockUserRepository;
+
+    @Test
+    @DisplayName("Test Signup for a single user Success")
+    void SignUpSuccess(){
+
+        Users userOne=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        when(mockUserRepository.findByEmail("haseb@gmail.com")).thenReturn(Optional.empty());
+        when(mockpasswordValidator.isValid("12345")).thenReturn(true);
+        when(mockpasswordEncoder.encode("12345")).thenReturn("12345");
+        when(mockUserRepository.save(userOne)).thenReturn(userOne);
+        Users exceptedUser=authService.signUp(userOne);
+        Assertions.assertSame(exceptedUser,userOne,"Two User should be same");
+    }
+
+    @Test
+    @DisplayName("Test Signup for a single user Failed")
+    void SignUpFailed(){
+
+        Users userOne=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        when(mockUserRepository.findByEmail("haseb@gmail.com")).thenReturn(Optional.empty());
+        when(mockpasswordValidator.isValid("12345")).thenReturn(false);
+        Assertions.assertThrows(InvalidPasswordException.class,()-> authService.signUp(userOne),"This should throw exception");
+    }
+
+    @Test
+    @DisplayName("Test SignIn for a single user Success")
+    void SignInSuccess(){
+
+        Users userOne=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        when(mockUserRepository.findByEmail("haseb@gmail.com")).thenReturn(Optional.of(userOne));
+        when(mockpasswordEncoder.matches("12345","12345")).thenReturn(true);
+        Users exceptedUser=authService.signIn(userOne);
+        Assertions.assertSame(exceptedUser,userOne,"Two User should be same");
+    }
+
+    @Test
+    @DisplayName("Test SignIn for a single user Failed")
+    void SignInFailed(){
+
+        Users userOne=new Users(1,"haseb@gmail.com","12345","Haseb","01789533586");
+        when(mockUserRepository.findByEmail("haseb@gmail.com")).thenReturn(Optional.of(userOne));
+        when(mockpasswordEncoder.matches("12345","12345")).thenReturn(false);
+        Assertions.assertThrows(AccessDeniedException.class,()->authService.signIn(userOne),"This should throw exception");
+    }
+}
